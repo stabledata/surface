3520021d95a7b7fe44f53d74ab4e409469c71453
cif <email@benipsen.com>
2024-04-17 22:24:12 +0100

    build: :loud_sound: turn on sourcemaps

--- routes/docs.tsx
+++ routes/docs.tsx
@@ -13,42 +13,42 @@ function Docs() {
         Field Notes
       </h2>
       <h3 className="mt-5">Principals</h3>
-      <p className="my-5">
+      <div className="my-5">
         Consciously working to reduce complexity is paramount to any software
         project, but perhaps even more so when we begin to blur the lines
         between client and server. Javascript has afforded us this ability, but
         we must wield the power responsibly.
-      </p>
+      </div>
 
       <h4 className="mt-5 text-md font-semibold">No Magic</h4>
-      <p className="my-5 text-sm leading-6">
+      <div className="my-5 text-sm leading-6">
         Nothing is abstracted away. This is a pattern for building web clients.
         There is no library, SDK, framework or anything more than a starting
         point. You can use it as a reference or a head start on your next
         project.
-      </p>
+      </div>
 
       <h4 className="mt-5 text-md font-semibold">Context Injection</h4>
-      <p className="my-5 text-sm leading-6">
+      <div className="my-5 text-sm leading-6">
         Surface wraps both Hono and Tanstack routers with a dead simple
         injection pattern that makes testing a breeze and the nuances between
         client and server easier to reason about.
-      </p>
+      </div>
 
       <h4 className="mt-5 text-md font-semibold">
         Single Service BFF and Client
       </h4>
-      <p className="my-5 text-sm leading-6">
+      <div className="my-5 text-sm leading-6">
         Surface is deployed as a NodeJS (or Bun) app container with a{" "}
         <a href="https://hono.dev/">Hono</a> backend. Routes are first matched
         by Hono and the catch all is delegated to{" "}
         <a href="https://tanstack.com/router/latest">Tanstack Router</a> and
         renders a client that builds and runs a dev server on{" "}
         <a href="https://vitejs.dev/">Vite.</a>.
-      </p>
+      </div>
 
       <h4 className="mt-5 text-md font-semibold">Flexible Routing</h4>
-      <p className="my-5 text-sm leading-6">
+      <div className="my-5 text-sm leading-6">
         Routes are entirely flexible. You don't have to prefix a backend request
         with <pre className="inline-block">/api</pre>, unless of course you want
         to. If Hono matches a route, say to <pre className="inline">/auth</pre>{" "}
@@ -59,10 +59,10 @@ function Docs() {
         <a href="https://tanstack.com/router/v1/docs/framework/react/guide/file-based-routing">
           file system based pattern.
         </a>
-      </p>
+      </div>
 
       <h4 className="mt-5 text-md font-semibold">Loaders</h4>
-      <p className="my-5 text-sm leading-6">
+      <div className="my-5 text-sm leading-6">
         The loader paradigm follows the render as you fetch pattern. Newer
         frameworks like Remix, Astro, Modern and more have elected to pursue
         this way, for good reason. Loaders in our case are{" "}
@@ -77,10 +77,10 @@ function Docs() {
         on the service and using RPC on the client. Ultimately the decision to
         keep this simple as possible made sense and we treat loaders as a single
         code path that works on both.
-      </p>
+      </div>
 
       <h4 className="mt-5 text-md font-semibold">Application State and SSR</h4>
-      <p className="my-5 text-sm leading-6">
+      <div className="my-5 text-sm leading-6">
         One of the biggest benefits of the surface pattern, is that we can store
         auth in httpOnly cookies for quick session lookups and <i>somewhat</i>{" "}
         seamlessly pass state between the client and server. <br />
@@ -89,7 +89,7 @@ function Docs() {
         the most sense was to take advantage of the Tanstack router's ability to
         dehydrate and rehydrate state by injecting sever session data directly
         through the router.
-      </p>
+      </div>
     </div>
   );
 }
