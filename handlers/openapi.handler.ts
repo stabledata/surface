93ddc518ad8b1ae7b044929eb567b410f33165d9
cif <email@benipsen.com>
2025-04-09 23:52:49 +0100

    feat: :memo: add swagger UI, neat

--- handlers/openapi.handler.ts
+++ handlers/openapi.handler.ts
@@ -1,24 +1,27 @@
 import { Hono } from "hono";
 import { openAPISpecs } from "hono-openapi";
 import { SurfaceEnv } from "../surface.app.ctx";
+import { swaggerUI } from "@hono/swagger-ui";
 
 export const bindOpenAPIRouteToApp = (
   app: Hono<SurfaceEnv>,
   endpoint: string = "/spec"
 ) => {
-  app.get(
-    endpoint,
-    openAPISpecs(app, {
-      documentation: {
-        info: {
-          title: "Surface API",
-          version: "1.0.0",
-          description: "Surface Template/Demo API",
+  app
+    .get(
+      endpoint,
+      openAPISpecs(app, {
+        documentation: {
+          info: {
+            title: "Surface API",
+            version: "1.0.0",
+            description: "Surface Template/Demo API",
+          },
+          servers: [
+            { url: "http://localhost:4000", description: "Local Dev Server" },
+          ],
         },
-        // servers: [
-        //   { url: "http://localhost:3000", description: "Local Server" },
-        // ],
-      },
-    })
-  );
+      })
+    )
+    .get("/ui", swaggerUI({ url: "/spec" }));
 };
